f25da037bf696a99727db8652d02bfcd081756c8
ryabykhms <ryabykhms@gmail.com>
2020-09-29 14:59:09 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const strArray = str.split("");
+    const openBrackets = [];
+    const closeBrackets = [];
+    bracketsConfig.forEach((rule) => {
+        openBrackets.push(rule[0]);
+        closeBrackets.push(rule[1]);
+    });
+    const stack = [];
+    const isValid = !strArray.some((char) => {
+        const isOpenUnlikeClose =
+            openBrackets.includes(char) &&
+            openBrackets.includes(char) !== closeBrackets.includes(char);
+        const isOpenLikeClose =
+            openBrackets.includes(char) &&
+            openBrackets.includes(char) === closeBrackets.includes(char);
+        const isStackIncludeOpen = stack.includes(openBrackets.indexOf(char));
+        if (isOpenUnlikeClose || (isOpenLikeClose && !isStackIncludeOpen)) {
+            stack.push(openBrackets.indexOf(char));
+        } else if (closeBrackets.includes(char)) {
+            if (
+                stack &&
+                stack[stack.length - 1] === closeBrackets.indexOf(char)
+            ) {
+                stack.pop();
+            } else {
+                return true;
+            }
+        }
+    });
+
+    return stack.length === 0 && isValid;
+};
